f7cf8fdfc7f667c74e18fd0e461a72153f53ae48
Ryan Huber <rhuber@gmail.com>
2019-11-23 20:13:26 +0000

    single key working

--- main.go
+++ main.go
@@ -238,9 +238,17 @@ func Main(configPath string, configTest bool, buildVersion string) {
 
 	handshakeManager := NewHandshakeManager(tunCidr, preferredRanges, hostMap, lightHouse, udpServer)
 
-	//TODO: These will be reused for psk
-	//handshakeMACKey := config.GetString("handshake_mac.key", "")
-	//handshakeAcceptedMACKeys := config.GetStringSlice("handshake_mac.accepted_keys", []string{})
+	//TODO: Add support for multiple psk/rotation
+	pskString := config.GetString("psk.key", "")
+	psk := []byte{}
+	if pskString != "" {
+		psk, err = sha256KdfFromString(pskString)
+		if err != nil {
+			l.Fatal(err)
+		}
+	} else {
+		psk = nil
+	}
 
 	checkInterval := config.GetInt("timers.connection_alive_interval", 5)
 	pendingDeletionInterval := config.GetInt("timers.pending_deletion_interval", 10)
@@ -258,6 +266,7 @@ func Main(configPath string, configTest bool, buildVersion string) {
 		pendingDeletionInterval: pendingDeletionInterval,
 		DropLocalBroadcast:      config.GetBool("tun.drop_local_broadcast", false),
 		DropMulticast:           config.GetBool("tun.drop_multicast", false),
+		PSK:                     psk,
 		UDPBatchSize:            config.GetInt("listen.batch", 64),
 	}
 
